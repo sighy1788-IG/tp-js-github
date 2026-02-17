99a27939ee10c4c4de43b99e6e1304adce0d3aca
Salma Ighy <s.ighy1788@uca.ac.ma>
2026-02-17 15:19:27 +0000

    hello

---
+++ exercices/ex1_variables.js
@@ -0,0 +1,9 @@
+fonction infosEtudiant (nom, note){
+    if (note >= 10){
+        return nom + " : " + note + "/20 => Admis";
+    }
+    else{
+        return nom + " : " + note + "/20 => Ajourné"; 
+    }
+}
+console.log(infosEtudiant("Salma" , 16));
